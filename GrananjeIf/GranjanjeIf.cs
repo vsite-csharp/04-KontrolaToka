b818d4582ebb7fa3ff8063930c0a5c6df8d6c021
Domagoj Krmpotić <domagojkrmpoti3@gmail.com>
2019-10-31 21:16:54 +0100

    Resolve if

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -34,6 +34,18 @@ namespace Vsite.CSharp.KontrolaToka
             // 3. provjeriti je li broj djeljiv s 5. 
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 5", a inače
             // 4. treba ispisati "broj nije djeljiv s 2, 3 niti 5"
+            if (broj % 2 == 0)
+            {
+                Console.WriteLine("broj je djeljiv s 2");
+            }
+            else if(broj% 3==0)
+                Console.WriteLine("broj je djeljiv s 3");
+
+            else if (broj % 5== 0)
+                Console.WriteLine("broj je djeljiv s 5");
+            else
+                Console.WriteLine("broj nije djeljiv s 2, 3 niti 5");
+
         }
     }
 }
