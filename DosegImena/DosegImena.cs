2f149a916d4dad2e4d3943c36f6e9f6afd4a1074
Senad Mandžić <semandzi@vsite.hr>
2019-10-31 21:19:01 +0100

    Resolve branching

--- DosegImena/DosegImena.cs
+++ DosegImena/DosegImena.cs
@@ -21,7 +21,7 @@ namespace Vsite.CSharp.KontrolaToka
             Console.WriteLine(a);
             Console.WriteLine(this.a);
             {
-                // TODO:012 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
+                // todo:012 definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
                 //int a = 5;
             }
         }
--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -28,12 +28,22 @@ namespace Vsite.CSharp.KontrolaToka
         {
             // TODO:020 Napisati grananja if koja će za uneseni broj:
             // 1. provjeriti je li broj paran. 
+            if (broj % 2 == 0)
+                Console.WriteLine("Broj je djeljiv sa 2");
             //    Ako je broj paran, treba ispisati poruku "broj je djeljiv s 2", a inače će
-            // 2. provjeriti je li broj djeljiv s 3. 
+            // 2. provjeriti je li broj djeljiv s 3.
+           else if (broj % 3 == 0)
+                Console.WriteLine("Broj je djeljiv sa 3");
+
+
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 3", a inače će
             // 3. provjeriti je li broj djeljiv s 5. 
+            else if (broj % 5== 0)
+                Console.WriteLine("Broj je djeljiv sa 5");
+
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 5", a inače
             // 4. treba ispisati "broj nije djeljiv s 2, 3 niti 5"
-        }
-    }
+            else
+                Console.WriteLine("Broj nije djeljiv sa 2,3,5 )
+        }    }
 }
