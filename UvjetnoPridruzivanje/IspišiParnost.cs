297446e1a3e42fbbf61a9c07bae5a98c256cefaa
Domagoj Salkić <dosalkic@vsite.hr>
2019-11-03 18:48:19 +0100

    Resolve if and ternary if

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -8,7 +8,7 @@ namespace Vsite.CSharp.KontrolaToka
         {
             // TODO:040 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
             string odgovor = "neparan";
-
+            odgovor = broj % 2 == 0 ? "paran":"neparan";
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
@@ -16,7 +16,8 @@ namespace Vsite.CSharp.KontrolaToka
         {
             // TODO:041 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
             string odgovor = "neparan";
-
+            if (broj % 2 == 0)
+                odgovor = "paran";
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
     }
