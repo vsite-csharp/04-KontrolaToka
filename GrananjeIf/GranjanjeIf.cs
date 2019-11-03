1b25f80d387e21b6d2ef63299a7234972f1feb12
Ivan Blažeka <ivblazek@vsite.hr>
2019-11-03 22:14:25 +0100

    Resolve comparison

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -26,7 +26,6 @@ namespace Vsite.CSharp.KontrolaToka
 
         public static void ProvjeriDjeljivost(int broj)
         {
-            // TODO:020 Napisati grananja if koja će za uneseni broj:
             // 1. provjeriti je li broj paran. 
             //    Ako je broj paran, treba ispisati poruku "broj je djeljiv s 2", a inače će
             if (broj % 2 == 0)
