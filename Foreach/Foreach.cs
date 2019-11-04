8f3137a145342f9a6971625f77f503c5418d5d5a
Ivan Blažeka <ivblazek@vsite.hr>
2019-11-04 16:38:46 +0100

    Resolve foreach

--- Foreach/Foreach.cs
+++ Foreach/Foreach.cs
@@ -8,8 +8,10 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void Ispiši(IEnumerable kolekcija)
         {
-            // TODO:110 Napisati foreach petlju tako da ispisuje sve članove kolekcije
-
+            foreach(object item in kolekcija)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         public static void Main(string[] args)
