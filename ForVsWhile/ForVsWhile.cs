b806839b2648995b411c02d0a75d3f1afff9b463
Igor Obradovic <iobradovic@ZAWMIOB>
2019-11-05 13:10:00 +0100

    Solve TODO in *ForVsWhile*.

--- ForVsWhile/ForVsWhile.cs
+++ ForVsWhile/ForVsWhile.cs
@@ -6,12 +6,23 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void PetljaFor(int n)
         {
-            // TODO:080 Napisati petlju for koja će ispisati brojeve od 0 do (uključivo) n
+            for(int i = 0; i <= n; ++i)
+            {
+                Console.Write(i); Console.Write(" ");
+            }
+            Console.WriteLine();
         }
 
         public static void PetljaWhile(int n)
         {
-            // TODO:081 Napisati petlju while koja će ispisati brojeve od 0 do (uključivo) n
+            int i = 0;
+            while(i <= 10)
+            {
+                Console.Write(i); Console.Write(" ");
+                ++i;
+            }
+            Console.WriteLine();
+
         }
 
         static void Main(string[] args)
@@ -19,8 +30,6 @@ namespace Vsite.CSharp.KontrolaToka
             PetljaFor(10);
 
             PetljaWhile(10);
-
-            // TODO:082 Prevesti program te programom ILDasm usporediti međukod gornjih metoda
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
