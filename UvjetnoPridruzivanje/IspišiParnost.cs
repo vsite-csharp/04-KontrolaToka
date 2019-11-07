778c0b249eb3acd2b96a8446ef6f9dcb0e14c60f
Domagoj Salkić <dosalkic@vsite.hr>
2019-11-07 19:29:07 +0100

    Resolve if and ternary if

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -7,14 +7,15 @@ namespace Vsite.CSharp.KontrolaToka
         public static void UvjetnimPridruživanjem(int broj)
         {
             string odgovor = "neparan";
-
+            odgovor = broj % 2 == 0 ? "paran" : "neparan";
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
         public static void GrananjemIf(int broj)
         {
             string odgovor = "neparan";
-
+            if (broj % 2 == 0)
+                odgovor = "paran";
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
     }
