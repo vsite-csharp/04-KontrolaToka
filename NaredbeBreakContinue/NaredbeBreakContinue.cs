cc390b5068066cad79508be55fdfe5dbdf9efd7d
Bernard Šetinc <besetinc@vsite.hr>
2019-11-08 17:07:11 +0100

    Solve break and continue

--- NaredbeBreakContinue/NaredbeBreakContinue.cs
+++ NaredbeBreakContinue/NaredbeBreakContinue.cs
@@ -9,7 +9,8 @@ namespace Vsite.CSharp.KontrolaToka
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO:100 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                if (i % d != 0)
+                    continue;
 
                 zbroj += i;
             }
@@ -21,11 +22,12 @@ namespace Vsite.CSharp.KontrolaToka
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO:101 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                if (i % d != 0)
+                    continue;
 
 
-                // TODO:102 Ako je i djeljiv sa d2, tada treba prekinuti petlju
-
+                if (i % d2 == 0)
+                    break;
 
                 zbroj += i;
             }
