e281382bbbccc83d646d9ed0c28e669428a75579
Bernard Šetinc <besetinc@vsite.hr>
2019-11-08 17:53:44 +0100

    Solve conditional join

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -6,8 +6,7 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void UvjetnimPridruživanjem(int broj)
         {
-            // TODO:040 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
-            string odgovor = broj % 2==0 ?"neparan" : "paran";
+            string odgovor = broj % 2==0 ?"paran" : "neparan";
             //if (broj % 2 == 0)
               //  odgovor = "paran";
 
@@ -16,8 +15,11 @@ namespace Vsite.CSharp.KontrolaToka
 
         public static void GrananjemIf(int broj)
         {
-            // TODO:041 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
-            string odgovor = "neparan";
+            string odgovor;
+            if (broj % 2 != 0)
+                odgovor = "neparan";
+            else
+                odgovor = "paran";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
