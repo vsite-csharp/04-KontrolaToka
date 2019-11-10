814757c0a133cfce7ab11fbc51e36efe8a3d8148
Emil Ordanic <emilordani@gmail.com>
2019-11-10 22:23:33 +0100

    Riješeno ForVsWhile

--- ForVsWhile/ForVsWhile.cs
+++ ForVsWhile/ForVsWhile.cs
@@ -6,17 +6,24 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void PetljaFor(int n)
         {
-            // TODO:080 Napisati petlju for koja će ispisati brojeve od 0 do (uključivo) n
+            for (int i = 0; i <= n; ++i)
+                Console.WriteLine(i);
         }
 
         public static void PetljaWhile(int n)
         {
-            Random rand = new Random();
-            int a = 0;
-            while ((a=rand.Next(5))!=0)
+            int i = 0;
+            while (i <= n)
             {
-                Console.WriteLine("a");
+                Console.WriteLine(i);
+                ++i;
             }
+        //    Random rand = new Random();
+        //    int a = 0;
+        //    while ((a=rand.Next(5))!=0)
+        //    {
+        //        Console.WriteLine("a");
+        //    }
         }
 
         static void Main(string[] args)
@@ -25,7 +32,6 @@ namespace Vsite.CSharp.KontrolaToka
 
             PetljaWhile(10);
 
-            // TODO:082 Prevesti program te programom ILDasm usporediti međukod gornjih metoda
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
