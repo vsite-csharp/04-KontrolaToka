7144bbce1f6183423ba700d618ca3a57e9e4ad68
Emil Ordanic <emilordani@gmail.com>
2019-11-10 21:47:25 +0100

    Riješeno NulStapajućiOperator

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -6,7 +6,6 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void UvjetnimPridruživanjem(int broj)
         {
-            // TODO:040 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
             string odgovor= broj % 2 ==0 ? "paran":"neparan";            
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
@@ -14,7 +13,6 @@ namespace Vsite.CSharp.KontrolaToka
 
         public static void GrananjemIf(int broj)
         {
-            // TODO:041 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
             string odgovor = "neparan";
             if (broj % 2 == 0)
                 odgovor = "paran";
