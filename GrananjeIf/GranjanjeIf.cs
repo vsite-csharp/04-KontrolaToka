5cb58f8cfb6008b1045f42317a919bed678f20af
Tomislav Peša <72340847+Topesa@users.noreply.github.com>
2020-11-05 21:05:39 +0100

    Resolve if-else if-else

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -26,16 +26,23 @@ namespace Vsite.CSharp.KontrolaToka
 
         public static void ProvjeriDjeljivost(int broj)
         {
-            // TODO:010 Napisati grananja if koja će za uneseni broj:
+            // Napisati grananja if koja će za uneseni broj:
             // 1. provjeriti je li broj paran. 
             //    Ako je broj paran, treba ispisati poruku "broj je djeljiv s 2", a inače će
+            if (broj % 2 == 0)
+                Console.WriteLine("broj je djeljiv s 2");
             // 2. provjeriti je li broj djeljiv s 3. 
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 3", a inače će
+            else if (broj % 3 == 0)
+                Console.WriteLine("broj je djeljiv s 3");
             // 3. provjeriti je li broj djeljiv s 5. 
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 5", a inače
+            else if (broj % 5 == 0)
+                Console.WriteLine("broj je djeljiv s 5");         
             // 4. treba ispisati "broj nije djeljiv s 2, 3 niti 5"
+            else Console.WriteLine("broj nije djeljiv s 2, 3 niti 5");
 
-            // TODO:011 Pokrenuti testove i provjertiti prolaze li 4 testa iz grupe TestGrananjaIf
+            // pokrenuti testove i provjertiti prolaze li 4 testa iz grupe TestGrananjaIf
         }
     }
 }
