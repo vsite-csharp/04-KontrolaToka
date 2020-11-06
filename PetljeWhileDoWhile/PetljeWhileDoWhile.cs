2f1a0bddaeacddfc132fd696bed5e79c7e0771ee
dajergov <dadoj@DESKTOP-4PTLH1S>
2020-11-06 17:39:49 +0100

    Resolve While ana Do while

--- PetljeWhileDoWhile/PetljeWhileDoWhile.cs
+++ PetljeWhileDoWhile/PetljeWhileDoWhile.cs
@@ -26,17 +26,17 @@ namespace Vsite.CSharp.KontrolaToka
             Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
 
             // Napisati petlju koja se ponavlja sve dok brojPređenihPolja ne postane jednak ili veći od 12
-            do
+            while (brojPređenihPolja<12)
             {
                 int bačeniBroj = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
                 Console.WriteLine($"Bacanje: {bačeniBroj}");
                 brojPređenihPolja += bačeniBroj;
                 Console.WriteLine($"Ukupno: {brojPređenihPolja}");
-            } while (brojPređenihPolja < 12);
+            }
             return brojPređenihPolja;
         }
 
-        // TODO:082 Pokrenuti testove i provjeriti prolaze li testovi u grupi TestPetljeWhileDoWhile
+        // Pokrenuti testove i provjeriti prolaze li testovi u grupi TestPetljeWhileDoWhile
         static void Main(string[] args)
         {
 
