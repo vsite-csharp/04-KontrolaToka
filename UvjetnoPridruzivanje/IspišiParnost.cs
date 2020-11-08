129a233ff4b5105b6b486e42f6f3da5e55e5ffaf
Kresimir Bratkovic <kbratkovic@gmail.com>
2020-11-08 18:20:21 +0100

    Resolve ternary operator

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -6,20 +6,26 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void UvjetnimPridruživanjem(int broj)
         {
-            // TODO:030 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
+            // Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
             string odgovor = "neparan";
+            odgovor = broj % 2 == 0 ? "paran" : "neparan";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
         public static void GrananjemIf(int broj)
         {
-            // TODO:031 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
+            //  Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
             string odgovor = "neparan";
+            if (broj % 2 == 0)
+                odgovor = "paran";
+            else
+                odgovor = "neparan";
+            
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
-        // TODO:032 Pokrenuti testove i provjeriti prolaze li oba testa iz grupe TestUvjetnogPridruživanja
+        //  Pokrenuti testove i provjeriti prolaze li oba testa iz grupe TestUvjetnogPridruživanja
     }
 }
