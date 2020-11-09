64e08e3799028ce6c298aa7bbf2ea8c9c733328a
Maja Smolković <masmolko@vsite.hr>
2020-11-09 13:05:50 +0100

    Resolve If-else and operator ?:

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -8,7 +8,8 @@ namespace Vsite.CSharp.KontrolaToka
         {
             // TODO:030 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
             string odgovor = "neparan";
-
+            
+            odgovor = (broj % 2 == 0) ? "paran" : "neparan";
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
@@ -16,6 +17,10 @@ namespace Vsite.CSharp.KontrolaToka
         {
             // TODO:031 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
             string odgovor = "neparan";
+            if (broj % 2 == 0)
+                odgovor = "paran";
+            else if (broj % 2 != 0)
+                odgovor = "neparan";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
