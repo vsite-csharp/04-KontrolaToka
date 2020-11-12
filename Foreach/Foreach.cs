c6e204708eccafd1bbe64af2e7c2dcf918d5b867
Jakov Zorić <jazoric@vsite.hr>
2020-11-12 19:27:28 +0100

    Resolve foreach loop

--- Foreach/Foreach.cs
+++ Foreach/Foreach.cs
@@ -8,11 +8,12 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void Ispiši(IEnumerable kolekcija)
         {
-            // TODO:100 Napisati foreach petlju tako da ispisuje sve članove kolekcije
-
+            foreach(var član in kolekcija)
+            {
+                Console.WriteLine(član);
+            }
         }
 
-        // TODO:101 Pokrenuti testove i provjeriti prolaze li oba testa u grupi TestForeach
 
         public static void Main(string[] args)
         {
