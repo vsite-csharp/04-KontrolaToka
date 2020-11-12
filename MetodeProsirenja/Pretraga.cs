2892c53210e9729192cdf287d680ebda36dd12bb
tirupcic <tirupcic@vsite.hr>
2020-11-12 19:56:22 +0100

    Resolve extension method

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -9,26 +9,43 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            //  Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
-            //test
-            throw new NotImplementedException();
-        }
 
+            //foreach (var rijec in nizRiječi)
+            //{
+            //    if(rijec == tražena)
+            //        return true;
+            //}
+            //    return false;
+
+            //throw new NotImplementedException();
+
+            return nizRiječi.Contains(tražena);
+        }
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
-
-            throw new NotImplementedException();
+            // Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+           return  brojevi.Sum();
+            //throw new /*NotImplementedException*/();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            List<string> rez = new List<string>();
 
+            foreach(var rijec in nizRiječi)
+            {
+                if (rijec.CompareTo(graničnaRiječ) > 0)
+                {
+                    rez.Add(rijec);
+                }
+            }
+            // Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            nizRiječi.Where(r => r.CompareTo(graničnaRiječ) > 0; /*lambda*/
             throw new NotImplementedException();
         }
 
-        // TODO:113 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        //  Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
