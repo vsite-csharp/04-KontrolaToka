c71a65c51aa3613a36647d128481639e5d9c138b
Jasmin Husić <jhsc1988@gmail.com>
2020-11-15 22:48:43 +0100

    Resolve decimal

--- UsporedbeDecimalnihBrojeva/UsporedbeDecimalnihBrojeva.cs
+++ UsporedbeDecimalnihBrojeva/UsporedbeDecimalnihBrojeva.cs
@@ -8,17 +8,24 @@ namespace Vsite.CSharp.KontrolaToka
         {
             double tri = 3.0;
 
-            // TODO:020 Provjeriti što će ispisati donje dvije usporedbe te promijeniti metodu JednakiSu tako da se dobije očekivani rezultat
-            // TODO:021 Pokrenuti testove i provjeriti prolaze li 2 testa iz grupe TestUsporedbeDecimalnihBrojeva
-            if (JednakiSu((1.0 - 1.0 / tri), (2.0 / tri)))
-                Console.WriteLine("1 - 1/3 jednako je 2/3");
+            // 020 Provjeriti što će ispisati donje dvije usporedbe te promijeniti metodu JednakiSu tako da se dobije očekivani rezultat
+            
+            // zbog pohranjivanja decimalnih brojeva u binarnom sustavu često dolazi do zaokruživanja
+            // i to može dovesti do neočekivanih rezultata
+            // to je posljedica načina pohranjivanja decimalnih brojeva u memoriji
+
+
+            // 021 Pokrenuti testove i provjeriti prolaze li 2 testa iz grupe TestUsporedbeDecimalnihBrojeva
+
+            if (JednakiSu((1.0 - 1.0 / tri), (2.0 / tri)))      // matematički bi trebalo biti jednako
+                Console.WriteLine("1 - 1/3 jednako je 2/3");    // ispisuje "1 - 1/3 nije jednako 2/3!"
             else
                 Console.WriteLine("1 - 1/3 nije jednako 2/3!");
 
             if (JednakiSu(tri * 0.1, 0.3))
                 Console.WriteLine("3 * 0.1 je jednako 0.3");
             else
-                Console.WriteLine("3 * 0.1 nije jednako 0.3!");
+                Console.WriteLine("3 * 0.1 nije jednako 0.3!"); // <- ovo ispisuje
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
@@ -26,7 +33,11 @@ namespace Vsite.CSharp.KontrolaToka
 
         public static bool JednakiSu(double broj1, double broj2)
         {
-            return broj1 == broj2;
+            if (broj1 == broj2)
+                return true;
+            return Math.Abs(broj1 - broj2) < (Math.Abs(broj1) + Math.Abs(broj2)) * 0.000001;  // tu usporedjujemo je li ta razlika manja od nekakve relativne pogreške
+                                                                                              // jer se kod usporedbe ne smijemo osloniti na to jesu li potpuno jednaki
+                                                                                              // sada ispisuje da su stvarno jednaki
         }
     }
 }
