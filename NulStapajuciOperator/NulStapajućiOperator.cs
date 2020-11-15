e5a8656ee8872277f5772cd2fbd61fd3560ee5ca
Jasmin Husić <jhsc1988@gmail.com>
2020-11-15 22:59:45 +0100

    Resolve null-coalescing operator

--- NulStapajuciOperator/NulStapajućiOperator.cs
+++ NulStapajuciOperator/NulStapajućiOperator.cs
@@ -6,11 +6,14 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void IspišiString(object obj)
         {
-            // TODO:040 Korištenjem Nul-stapajućeg (null-coalescing) operatora ?? ispisati sadržaj objekta ako je različit od null, inače ispisati "Nedefiniran"
+            // 040 Korištenjem Nul-stapajućeg (null-coalescing) operatora ?? ispisati sadržaj objekta ako je različit od null, inače ispisati "Nedefiniran"
+            // null-coalescing operator je specifičan za CSH - on če vratiti objekt ako je objekt različit od null, u protivnom će vratiti neku drugu vrijednost
 
+            // Console.WriteLine( obj != null ? obj : "Nedefiniran");
+            Console.WriteLine(obj ?? "Nedefiniran"); // ovo su identične operacije
         }
 
-        // TODO:041 Pokrenuti testove i provjeriti prolaze li sva 4 testa iz grupe TestNulStapajućegOperatora
+        // 041 Pokrenuti testove i provjeriti prolaze li sva 4 testa iz grupe TestNulStapajućegOperatora
 
         static void Main(string[] args)
         {
