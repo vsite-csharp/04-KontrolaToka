0d6b82847c85659c220aafc536e5b9738739cf1b
robilovric <robilovric7@gmail.com>
2021-10-28 21:10:31 +0200

    Resolve variable scope

--- DosegImena/DosegImena.cs
+++ DosegImena/DosegImena.cs
@@ -17,15 +17,17 @@ namespace Vsite.CSharp.KontrolaToka
         {
             string a = "Lokalna varijabla";
 
-            // TODO:000 Ispisati vrijednosti lokalne varijable a
+            //:000 Ispisati vrijednosti lokalne varijable a
+            Console.WriteLine(a);
 
-            // TODO:001 Ispisati vrijednost člana klase a
+            //:001 Ispisati vrijednost člana klase a
+            Console.WriteLine(this.a);
 
             {
-                // TODO:002 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
-
+                //:002 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
+                //int a = 7;
             }
-            // TODO:003 Pokrenuti testove i provjeriti je li prošao test u grupi TestDosegaImena
+            //:003 Pokrenuti testove i provjeriti je li prošao test u grupi TestDosegaImena
         }
 
         string a = "Član klase";
