f10d4f12bf1928e7d2d73c0f92e76651bd885463
Andrea Vrljic <andrea31542@gmail.com>
2021-10-29 12:02:00 +0200

    Done 110-114 MetodeProširenja

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -9,28 +9,38 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            // 110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
-
-            throw new NotImplementedException();
+            return nizRiječi.Contains(tražena); 
+            
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            // 111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            return brojevi.Sum(); 
 
-            throw new NotImplementedException();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            // 112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            IEnumerable<string> elements = new string[] {};
+            nizRiječi = nizRiječi.OrderBy(el => el);
 
-            throw new NotImplementedException();
+            foreach (string word in nizRiječi)
+            {
+                if (word.CompareTo(graničnaRiječ) > 0)
+                {
+                    elements = elements.Concat(new [] {word});
+                }
+            }
+
+            return elements;
         }
 
-        // TODO:113 Pokrenuti program i provjeriti ispise
+        // 113 Pokrenuti program i provjeriti ispise
         
-        // TODO:114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        // 114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
