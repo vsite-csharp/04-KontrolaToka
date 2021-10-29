3ccf5c2319c5d7e7ebcbabb4cc10d42c6c414d2f
Pavle Pavicic <PavleP@dspace.hr>
2021-10-29 10:00:40 +0200

    Resolve all of TODO:01*.

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -26,7 +26,29 @@ namespace Vsite.CSharp.KontrolaToka
 
         public static void ProvjeriDjeljivost(int broj)
         {
-            // TODO:010 Napisati grananja if koja će za uneseni broj:
+            // Napisati grananja if koja će za uneseni broj:
+            if (broj % 2 == 0)
+            {
+                Console.WriteLine("broj je djeljiv s 2");
+            }
+            else
+            {
+                if (broj % 3 == 0)
+                {
+                    Console.WriteLine("broj je djeljiv s 3");
+                }
+                else
+                {
+                    if (broj % 5 == 0)
+                    {
+                        Console.WriteLine("broj je djeljiv s 5");
+                    }
+                    else
+                    {
+                        Console.WriteLine("broj nije djeljiv s 2, 3 niti 5");
+                    }
+                }
+            }
             // 1. provjeriti je li broj paran. 
             //    Ako je broj paran, treba ispisati poruku "broj je djeljiv s 2" (KORISTI DOSLOVNO OVE STRINGOVE ZBOG TESTOVA), a inače će
             // 2. provjeriti je li broj djeljiv s 3. 
@@ -35,9 +57,9 @@ namespace Vsite.CSharp.KontrolaToka
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 5", a inače
             // 4. treba ispisati "broj nije djeljiv s 2, 3 niti 5"
 
-            // TODO:011 Pokrenuti program nekoliko puta i upisom različitih brojeva provjeriti ispravnost koda
+            // Pokrenuti program nekoliko puta i upisom različitih brojeva provjeriti ispravnost koda
 
-            // TODO:012 Pokrenuti testove i provjeriti prolaze li 4 testa iz grupe TestGrananjaIf
+            // Pokrenuti testove i provjeriti prolaze li 4 testa iz grupe TestGrananjaIf
         }
     }
 }
