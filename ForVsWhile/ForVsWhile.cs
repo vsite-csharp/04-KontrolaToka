4de84a895f44561da39d7d285dde69ee76d252fe
Pavle Pavicic <PavleP@dspace.hr>
2021-10-29 12:06:36 +0200

    Resolve all TODO:07*.

--- ForVsWhile/ForVsWhile.cs
+++ ForVsWhile/ForVsWhile.cs
@@ -6,12 +6,22 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static void PetljaFor(int n)
         {
-            // TODO:070 Napisati petlju for koja će ispisati brojeve od 0 do (uključivo) n
+            // Napisati petlju for koja će ispisati brojeve od 0 do (uključivo) n
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
 
         public static void PetljaWhile(int n)
         {
-            // TODO:071 Napisati petlju while koja će ispisati brojeve od 0 do (uključivo) n
+            // Napisati petlju while koja će ispisati brojeve od 0 do (uključivo) n
+            int i = 0;
+            while (i <= n)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
         }
 
         static void Main(string[] args)
@@ -25,10 +35,10 @@ namespace Vsite.CSharp.KontrolaToka
             Console.ReadKey();
         }
 
-        // TODO:072 Pokrenuti program te provjeriti ispise
+        // Pokrenuti program te provjeriti ispise
 
-        // TODO:073 Programom ILDasm (Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N Tools\ildasm.exe) usporediti međukod metoda PetljaFor i PetljaWhile
+        // Programom ILDasm (Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N Tools\ildasm.exe) usporediti međukod metoda PetljaFor i PetljaWhile
 
-        // TODO:074 Pokrenuti testove i projeriti prolaze li oba testa u grupi TestForVsWhile
+        // Pokrenuti testove i projeriti prolaze li oba testa u grupi TestForVsWhile
     }
 }
