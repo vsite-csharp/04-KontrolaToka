9f43ae828f7336fc9f518487004ec6df3a2d9680
Andrea Vrljic <andrea31542@gmail.com>
2021-10-29 08:01:42 +0200

    Done 050-051 NulUvjetniOperator

--- NulUvjetniOperator/NulUvjetniOperator.cs
+++ NulUvjetniOperator/NulUvjetniOperator.cs
@@ -8,14 +8,13 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static string PozoviToString(object obj)
         {
-            // TODO:050 Korištenjem nul-uvjetnog (null conditional) operatora ?. vratiti rezultat metode ToString na prosljeđenom objektu ako je različit od nule, u protivnom vratiti nul-referencu
-
-            throw new NotImplementedException();
+            // Sqrt050 Korištenjem nul-uvjetnog (null conditional) operatora ?. vratiti rezultat metode ToString na prosljeđenom objektu ako je različit od nule, u protivnom vratiti nul-referencu
+            return obj?.ToString();
         }
 
-        // TODO:051 Pokrenuti program i provjeriti ispis
+        // Sqrt051 Pokrenuti program i provjeriti ispis
 
-        // TODO:052 Pokrenuti testove i provjeriti prolaze li sva 3 testa iz grupe TestNulUvjetnogOperatora
+        // Sqrt052 Pokrenuti testove i provjeriti prolaze li sva 3 testa iz grupe TestNulUvjetnogOperatora
 
         static void Main(string[] args)
         {
