902475bfee986860cd5b1d138745f61ecf10a73c
robilovric <robilovric7@gmail.com>
2021-11-02 00:29:06 +0100

    Resolve MetodeProširenja

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -9,28 +9,52 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            //:110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
-
-            throw new NotImplementedException();
+            foreach(string el in nizRiječi)
+            {
+                if (el.ToLower() == tražena.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+            //throw new NotImplementedException();
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
-
-            throw new NotImplementedException();
+            //:111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            int suma = 0;
+            foreach(int el in brojevi)
+            {
+                suma += el;
+            }
+            return suma;
+            //throw new NotImplementedException();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
-
-            throw new NotImplementedException();
+            //:112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            int counter = 0;
+            var temp = nizRiječi.ToList();
+            List<int> to_del = new List<int>();
+            foreach(string el in nizRiječi)
+            {
+                if (el.CompareTo(graničnaRiječ)<0)
+                {
+                    temp.RemoveAt(counter);
+                    continue;
+                }
+                counter++;
+            }
+            return temp;
+            //throw new NotImplementedException();
         }
 
-        // TODO:113 Pokrenuti program i provjeriti ispise
+        //:113 Pokrenuti program i provjeriti ispise
         
-        // TODO:114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        //:114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
