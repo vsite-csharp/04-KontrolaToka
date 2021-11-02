336f851b70d84d3a1bbf691c1be9582a9a588f29
Ana Lovric <alovric@dspace.hr>
2021-11-02 23:08:55 +0100

    DZ: Resloved Metode Proširenja.

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -9,7 +9,11 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            // 110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            if (nizRiječi.Contains(tražena))
+                return true;
+            else
+                return false;
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
 
             throw new NotImplementedException();
@@ -17,20 +21,36 @@ namespace Vsite.CSharp.KontrolaToka
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            // 111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            //int zbroj = 0;
+            //foreach (var broj in brojevi)
+            //{
+            //    zbroj = zbroj + broj;
+            //}
+            //return zbroj;
 
+            return brojevi.Sum();
             throw new NotImplementedException();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            // 112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            IEnumerable<string> novaKolekcija = new string[] { };
+            foreach (var riječ in nizRiječi)
+            {
+                if (riječ.CompareTo(graničnaRiječ) >0)
+                {
+                    novaKolekcija = novaKolekcija.Concat(new[] { riječ });
+                }
+            }
 
+            return novaKolekcija;
             throw new NotImplementedException();
         }
 
-        // TODO:113 Pokrenuti program i provjeriti ispise
+        // 113 Pokrenuti program i provjeriti ispise
         
-        // TODO:114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        // 114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
