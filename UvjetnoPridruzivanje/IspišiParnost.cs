f770707299f9df30c66a3e0c01122f6342e47a9f
Luka Šestak <lusestak@vsite.hr>
2022-01-27 13:01:31 +0100

    .

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -1,27 +1,31 @@
 ﻿using System;
 
+
 namespace Vsite.CSharp.KontrolaToka
 {
     static class IspišiParnost
     {
         public static void UvjetnimPridruživanjem(int broj)
         {
-            // TODO:030 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
-            string odgovor = "neparan";
+            // Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
+            string odgovor = (broj % 2 == 0) ? "paran" : "neparan";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
         public static void GrananjemIf(int broj)
         {
-            // TODO:031 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
-            string odgovor = "neparan";
+            // Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
+            string odgovor;
+            if (broj % 2 == 0) odgovor = "paran";
+            else odgovor = "neparan";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
 
-        // TODO:032 Pokrenuti program nekoliko puta i za različite unose provjeriti ispravnost koda
+        // Pokrenuti program nekoliko puta i za različite unose provjeriti ispravnost koda
 
-        // TODO:033 Pokrenuti testove i provjeriti prolaze li oba testa iz grupe TestUvjetnogPridruživanja
+        // Pokrenuti testove i provjeriti prolaze li oba testa iz grupe TestUvjetnogPridruživanja
     }
 }
+            
