207b0c073c2fd6d7c4bd65cbb9a373c466771400
Luka <Luka@DESKTOP-PUVMF0P>
2022-11-03 21:10:02 +0100

    Variable scope and if branch

--- DosegImena/DosegImena.cs
+++ DosegImena/DosegImena.cs
@@ -18,14 +18,16 @@ namespace Vsite.CSharp.KontrolaToka
             string a = "Lokalna varijabla";
 
             // TODO:000 Ispisati vrijednosti lokalne varijable a
-
+            Console.WriteLine(a);
             // TODO:001 Ispisati vrijednost člana klase a
-
+            Console.WriteLine(this.a);
             {
                 // TODO:002 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
-
+                //String a = "Varijabla a u bloku";
+                Console.WriteLine(a);    
             }
             // TODO:003 Pokrenuti testove i provjeriti je li prošao test u grupi TestDosegaImena
+            Console.WriteLine(a);
         }
 
         string a = "Član klase";
