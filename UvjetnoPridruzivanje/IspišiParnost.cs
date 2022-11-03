96c62a76fd9077e18d2246a296575cc0bc05b1eb
Luka <Luka@DESKTOP-PUVMF0P>
2022-11-03 21:25:37 +0100

    condidtional assignment

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -7,7 +7,10 @@ namespace Vsite.CSharp.KontrolaToka
         public static void UvjetnimPridruživanjem(int broj)
         {
             // TODO:030 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
+            
             string odgovor = "neparan";
+
+            odgovor = broj % 2 == 0 ? "paran" : "neparan";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
@@ -16,6 +19,15 @@ namespace Vsite.CSharp.KontrolaToka
         {
             // TODO:031 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
             string odgovor = "neparan";
+
+            if (broj % 2 == 0)
+            {
+                odgovor = "paran";
+            }
+            else
+            {
+                odgovor = "neparan";
+            }
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
