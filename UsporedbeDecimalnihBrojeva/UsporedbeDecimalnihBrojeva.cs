a0bef332c95003e17d850585112f1eb2bb06f7f8
PatrikBacic <patrikbacic@gmail.com>
2022-11-03 21:20:51 +0100

    Resolve floating point

--- UsporedbeDecimalnihBrojeva/UsporedbeDecimalnihBrojeva.cs
+++ UsporedbeDecimalnihBrojeva/UsporedbeDecimalnihBrojeva.cs
@@ -8,7 +8,7 @@ namespace Vsite.CSharp.KontrolaToka
         {
             double tri = 3.0;
 
-            // TODO:020 Provjeriti što će ispisati donje usporedbe.
+            // 020 Provjeriti što će ispisati donje usporedbe.
             IspišiJesuLiJednaki(1.0, 1.0001);
 
             IspišiJesuLiJednaki(-1e-25, -1.00001e-25);
@@ -23,7 +23,7 @@ namespace Vsite.CSharp.KontrolaToka
             {
                 Console.WriteLine("1 - 1/3 nije jednako 2/3!");
             }
-            //IspišiJesuLiJednaki((1.0 - 1.0 / tri), (2.0 / tri));
+            IspišiJesuLiJednaki((1.0 - 1.0 / tri), (2.0 / tri));
 
             if (JednakiSu(tri * 0.1, 0.3))
             { 
@@ -33,7 +33,7 @@ namespace Vsite.CSharp.KontrolaToka
             { 
                 Console.WriteLine("3 * 0.1 nije jednako 0.3!");
             }
-            //IspišiJesuLiJednaki(tri * 0.1, 0.3);
+            IspišiJesuLiJednaki(tri * 0.1, 0.3);
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
@@ -51,12 +51,16 @@ namespace Vsite.CSharp.KontrolaToka
             }
         }
 
-        // TODO:021 Promijeniti metodu JednakiSu tako da se dobije očekivani rezultat
+        // 021 Promijeniti metodu JednakiSu tako da se dobije očekivani rezultat
         public static bool JednakiSu(double broj1, double broj2)
         {
-            return broj1 == broj2;
+            if (broj1 == broj2)
+            {
+                return true;
+            }
+            return Math.Abs(broj1-broj2) < Math.Abs (broj1 + broj2) / 1e10;
         }
 
-        // TODO:022 Pokrenuti testove i provjeriti prolaze li 2 testa iz grupe TestUsporedbeDecimalnihBrojeva
+        // 022 Pokrenuti testove i provjeriti prolaze li 2 testa iz grupe TestUsporedbeDecimalnihBrojeva
     }
 }
