c4f2440877d345e54d01b6f7c9e48982d8016174
Giancarlo <giancarlo.bedek@hotmail.com>
2022-11-10 21:17:33 +0100

    Resolve while and do while loops

--- PetljeWhileDoWhile/PetljeWhileDoWhile.cs
+++ PetljeWhileDoWhile/PetljeWhileDoWhile.cs
@@ -10,13 +10,13 @@ namespace Vsite.CSharp.KontrolaToka
             int brojBacanja = 0;
             int bačeniBroj = 0;
 
-            // TODO:080 Napisati petlju koja ispisuje rezultat bacanja sve dok ne padne na 6
-
+            // Napisati petlju koja ispisuje rezultat bacanja sve dok ne padne na 6
+            while (bačeniBroj != 6)
             {
                 ++brojBacanja;
                 bačeniBroj = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
                 Console.WriteLine($"{brojBacanja}. pokušaj: {bačeniBroj}");
-            }
+            } 
 
             return brojBacanja;
         }
@@ -25,8 +25,8 @@ namespace Vsite.CSharp.KontrolaToka
         {
             Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
 
-            // TODO:081 Napisati petlju koja se ponavlja sve dok brojPređenihPolja ne postane jednak ili veći od 12
-
+            // Napisati petlju koja se ponavlja sve dok brojPređenihPolja ne postane jednak ili veći od 12
+            while (brojPređenihPolja < 12)
             {
                 int bačeniBroj = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
                 Console.WriteLine($"Bacanje: {bačeniBroj}");
@@ -36,7 +36,7 @@ namespace Vsite.CSharp.KontrolaToka
             return brojPređenihPolja;
         }
 
-        // TODO:082 Pokrenuti program nekoliko puta i provjeriti ispise
+        // Pokrenuti program nekoliko puta i provjeriti ispise
         static void Main()
         {
 
@@ -49,6 +49,6 @@ namespace Vsite.CSharp.KontrolaToka
             Console.ReadKey();
         }
 
-        // TODO:083 Pokrenuti testove i provjeriti prolaze li testovi u grupi TestPetljeWhileDoWhile
+        // Pokrenuti testove i provjeriti prolaze li testovi u grupi TestPetljeWhileDoWhile
     }
 }
