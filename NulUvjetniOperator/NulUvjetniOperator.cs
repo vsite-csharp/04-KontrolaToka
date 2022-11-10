ce6543642438b4fa42a6a4663142d2674a9fa9e4
Nedim Omerović <neomerov@vsite.hr>
2022-11-10 19:38:22 +0100

    Resolve null-conditional operator

--- NulUvjetniOperator/NulUvjetniOperator.cs
+++ NulUvjetniOperator/NulUvjetniOperator.cs
@@ -9,13 +9,13 @@ namespace Vsite.CSharp.KontrolaToka
         public static string PozoviToString(object obj)
         {
             // TODO:050 Korištenjem nul-uvjetnog (null conditional) operatora ?. vratiti rezultat metode ToString na prosljeđenom objektu ako je različit od nule, u protivnom vratiti nul-referencu
-
-            throw new NotImplementedException();
+            return obj?.ToString();
+            //throw new NotImplementedException();
         }
 
-        // TODO:051 Pokrenuti program i provjeriti ispis
+        // :051 Pokrenuti program i provjeriti ispis
 
-        // TODO:052 Pokrenuti testove i provjeriti prolaze li sva 3 testa iz grupe TestNulUvjetnogOperatora
+        // :052 Pokrenuti testove i provjeriti prolaze li sva 3 testa iz grupe TestNulUvjetnogOperatora
 
         static void Main(string[] args)
         {
