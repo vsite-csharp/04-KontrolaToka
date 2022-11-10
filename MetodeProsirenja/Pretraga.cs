9161ae69f265bea56b7b30af2a94c5f136832c31
Giancarlo <giancarlo.bedek@hotmail.com>
2022-11-10 20:53:00 +0100

    Resolve expansion methods

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -9,28 +9,35 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            //foreach (var riječ in nizRiječi)
+            //{
+            //    if (riječ == tražena)
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+
+            // Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
 
-            throw new NotImplementedException();
+            return nizRiječi.Contains(tražena);
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
-
-            throw new NotImplementedException();
+            // Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            return brojevi.Sum();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
-
-            throw new NotImplementedException();
+            // Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            return nizRiječi.Where(riječ => string.Compare(riječ, graničnaRiječ, true) > 0);
         }
 
-        // TODO:113 Pokrenuti program i provjeriti ispise
+        // Pokrenuti program i provjeriti ispise
         
-        // TODO:114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        // Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
