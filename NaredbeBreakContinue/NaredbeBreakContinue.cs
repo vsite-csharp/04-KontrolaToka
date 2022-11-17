189bbef813560e480e5d3d34551b84282ef85159
Marko Bohač <mabohac@vsite.hr>
2022-11-17 15:13:04 +0100

    Resolved break and continue

--- NaredbeBreakContinue/NaredbeBreakContinue.cs
+++ NaredbeBreakContinue/NaredbeBreakContinue.cs
@@ -9,8 +9,9 @@ namespace Vsite.CSharp.KontrolaToka
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO:090 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
-
+                // 090 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                if (i % d != 0)
+                    continue;
                 zbroj += i;
             }
             return zbroj;
@@ -21,22 +22,24 @@ namespace Vsite.CSharp.KontrolaToka
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO:091 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                // 091 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                if (i % d != 0)
+                    continue;
 
-
-                // TODO:092 Ako je i djeljiv sa d2, tada treba prekinuti petlju
-
+                // 092 Ako je i djeljiv sa d2, tada treba prekinuti petlju
+                if (i % d2 == 0)
+                    break;
 
                 zbroj += i;
             }
             return zbroj;
         }
 
-        // TODO:093 Pokrenuti program i provjeriti ispise
+        // 093 Pokrenuti program i provjeriti ispise
 
         static void Main(string[] args)
         {
-            Console.WriteLine(ZbrojiDoNSveKojiSuDjeljiviSaD(10, 2)); // trebalo bi ispisati 20
+            Console.WriteLine(ZbrojiDoNSveKojiSuDjeljiviSaD(10, 2)); // trebalo bi ispisati 30
             Console.WriteLine(ZbrojiDoNSveKojiSuDjeljiviSaD(10, 3)); // trebalo bi ispisati 18
             Console.WriteLine(ZbrojiDoNSveKojiSuDjeljiviSaD(10, 5)); // trebalo bi ispisati 15
 
@@ -46,6 +49,6 @@ namespace Vsite.CSharp.KontrolaToka
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
         }
-        // TODO:094 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestNaredbeBreakContinue
+        // 094 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestNaredbeBreakContinue
     }
 }
