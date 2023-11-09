4ce27d6b2e256c48f70a43be26aa9ebe01a83b10
Josip Grabušić <jograbus@vsite.hr>
2023-11-09 20:45:14 +0100

    Resolved pridruzivanje

--- UvjetnoPridruzivanje/IspišiParnost.cs
+++ UvjetnoPridruzivanje/IspišiParnost.cs
@@ -7,7 +7,7 @@ namespace Vsite.CSharp.KontrolaToka
         public static void UvjetnimPridruživanjem(int broj)
         {
             // TODO:030 Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
-            string odgovor = "neparan";
+            string odgovor = broj % 2 == 0 ? "paran" : "neparan";
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
@@ -15,7 +15,11 @@ namespace Vsite.CSharp.KontrolaToka
         public static void GrananjemIf(int broj)
         {
             // TODO:031 Korištenjem grananja if dodijeliti varijabli odgovor vrijednost "paran" ili "neparan" ovisno o djeljivosti zadanog broja
-            string odgovor = "neparan";
+           string odgovor = broj % 2 == 0 ? "paran" : "neparan";
+            if (broj % 2 == 0)
+            {
+                odgovor = "paran";
+            }
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
