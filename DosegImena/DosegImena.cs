0b3c8d189438214cb8136314276d1b65ad1ce534
Marko Pavić <mapavic@vsite.hr>
2023-11-09 19:57:05 +0100

    Resolve variable name scope

--- DosegImena/DosegImena.cs
+++ DosegImena/DosegImena.cs
@@ -16,15 +16,17 @@ namespace Vsite.CSharp.KontrolaToka
         {
             string a = "Lokalna varijabla";
 
-            // TODO:000 Ispisati vrijednosti lokalne varijable a
+            // 000 Ispisati vrijednosti lokalne varijable a
+            Console.WriteLine(a);
 
-            // TODO:001 Ispisati vrijednost člana klase a
-
+            // 001 Ispisati vrijednost člana klase a
+            Console.WriteLine(this.a);
             {
-                // TODO:002 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
-
+                // 002 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
+                //int a = 5;
+                //Console.WriteLine(a);
             }
-            // TODO:003 Pokrenuti testove i provjeriti je li prošao test u grupi TestDosegaImena
+            // 003 Pokrenuti testove i provjeriti je li prošao test u grupi TestDosegaImena
         }
 
         readonly string a = "Član klase";
