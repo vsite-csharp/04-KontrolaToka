f95e9ee8880fa8eed30ed7f43006eee11ea144e4
Kristian Mesić <kristian.mesic303@gmail.com>
2023-11-09 19:58:08 +0100

    - check switch/case

--- GrananjeSwitch/IspisDana.cs
+++ GrananjeSwitch/IspisDana.cs
@@ -6,11 +6,19 @@
         {
             switch (danUTjednu)
             {
-                // TODO:060 Napisati grane case za svaki dan u tjednu tako da vraćaju "ponedjeljak" za DayOfWeek.Monday, "utorak" za DayOfWeek.Tuesday itd.
+                // :060 Napisati grane case za svaki dan u tjednu tako da vraćaju "ponedjeljak" za DayOfWeek.Monday, "utorak" za DayOfWeek.Tuesday itd.
+                case DayOfWeek.Sunday: return "Nedjelja";
+                case DayOfWeek.Monday: return "Ponedjeljak";
+                case DayOfWeek.Tuesday: return "Utorak";
+                case DayOfWeek.Wednesday: return "Srijeda";
+                case DayOfWeek.Thursday: return "Četvrtak";
+                case DayOfWeek.Friday: return "Petak";
+                case DayOfWeek.Saturday: return "Subota";
 
-                // TODO:061 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
+
+                // :061 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
                 default:
-                    throw new NotImplementedException();
+                    throw new ArgumentOutOfRangeException(nameof(danUTjednu));
             }
         }
 
@@ -18,16 +26,18 @@
         {
             switch (danUTjednu)
             {
-                // TODO:062 Napisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
-
-                // TODO:063 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
+                // :062 Napisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
+                case DayOfWeek.Sunday: return "vikend";
+                case DayOfWeek.Saturday: return "vikend";
+                case DayOfWeek: return "radni dan";
+                // :063 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
                 default:
-                    throw new NotImplementedException();
+                    throw new ArgumentOutOfRangeException(nameof(danUTjednu));
             }
         }
 
-        // TODO:064 Pokrenuti program i provjeriti ispravnost ispisa
+        // :064 Pokrenuti program i provjeriti ispravnost ispisa
         
-        // TODO:065 Pokrenuti testove i provjeriti prolaze li svi testovi iz grupe TestGrananjaSwitch
+        // :065 Pokrenuti testove i provjeriti prolaze li svi testovi iz grupe TestGrananjaSwitch
     }
 }
