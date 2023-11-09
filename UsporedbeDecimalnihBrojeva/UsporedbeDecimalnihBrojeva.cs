f357b59888b934a15c4c2d1c0c195c691f032f94
Ivan Davidović <hedead421@gmail.com>
2023-11-09 20:23:52 +0100

    Resolve decimal number comparison

--- UsporedbeDecimalnihBrojeva/UsporedbeDecimalnihBrojeva.cs
+++ UsporedbeDecimalnihBrojeva/UsporedbeDecimalnihBrojeva.cs
@@ -6,7 +6,7 @@
         {
             double tri = 3.0;
 
-            // TODO:020 Provjeriti što će ispisati donje usporedbe.
+            // :020 Provjeriti što će ispisati donje usporedbe.
             IspišiJesuLiJednaki(1.0, 1.0001);
 
             IspišiJesuLiJednaki(-1e-25, -1.00001e-25);
@@ -48,12 +48,15 @@
             }
         }
 
-        // TODO:021 Promijeniti metodu JednakiSu tako da se dobije očekivani rezultat
+        // :021 Promijeniti metodu JednakiSu tako da se dobije očekivani rezultat
         public static bool JednakiSu(double broj1, double broj2)
         {
-            return broj1 == broj2;
+            if(broj1 == broj2){
+                return true;
+            }
+            return Math.Abs(broj1 - broj2) / Math.Abs(Math.Max(broj1, broj2)) < 1e-10;
         }
 
-        // TODO:022 Pokrenuti testove i provjeriti prolaze li 2 testa iz grupe TestUsporedbeDecimalnihBrojeva
+        // :022 Pokrenuti testove i provjeriti prolaze li 2 testa iz grupe TestUsporedbeDecimalnihBrojeva
     }
 }
