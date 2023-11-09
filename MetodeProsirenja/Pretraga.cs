48ba6fa4719b1f4bb8afcad7584da83f210bf069
Kristian Mesić <kristian.mesic303@gmail.com>
2023-11-09 20:58:46 +0100

    - check extending methods

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -1,31 +1,52 @@
-﻿namespace Vsite.CSharp.KontrolaToka
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace Vsite.CSharp.KontrolaToka
 {
     static class Pretraga
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            // :110 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
+            return nizRiječi.Contains<string>(tražena);
 
-            throw new NotImplementedException();
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            // :111 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            //int sum = 0;
+            //foreach(int i in brojevi)
+            //{
+            //    sum += i;
+            //}
+            //return sum;
 
-            throw new NotImplementedException();
+            return brojevi.Sum();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            List<string> Pronađene = new List<string>();
+            // :112 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
 
-            throw new NotImplementedException();
+            foreach (var item in nizRiječi)
+            {
+                if(String.Compare(item, graničnaRiječ) > 0)
+                {
+                    Console.WriteLine(item);
+                    Pronađene.Add(item);
+                }
+            }
+
+            return Pronađene;
+
+            //throw new NotImplementedException();
         }
 
-        // TODO:113 Pokrenuti program i provjeriti ispise
+        // :113 Pokrenuti program i provjeriti ispise
         
-        // TODO:114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        // :114 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
