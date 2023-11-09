4016bea0aeebe0d429b2e6277bb0b5617bc2db63
Kristian Bolarić <krbolaric@vsite.hr>
2023-11-09 21:06:13 +0100

    Resolve switch branching

--- GrananjeSwitch/IspisDana.cs
+++ GrananjeSwitch/IspisDana.cs
@@ -6,18 +6,6 @@
         {
             switch (danUTjednu)
             {
-                // TODO:060 Napisati grane case za svaki dan u tjednu tako da vraćaju "ponedjeljak" za DayOfWeek.Monday, "utorak" za DayOfWeek.Tuesday itd.
-
-                // TODO:061 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
-                default:
-                    throw new NotImplementedException();
-            }
-        }
-
-        public static string RadniNeradni(DayOfWeek danUTjednu)
-        {
-            switch (danUTjednu)
-            {
                 case DayOfWeek.Monday:
                     return "ponedjeljak";
                 case DayOfWeek.Tuesday:
@@ -32,8 +20,29 @@
                     return "subota";
                 case DayOfWeek.Sunday:
                     return "nedjelja";
-                // TODO:062 Napisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
+                // TODO:060 Napisati grane case za svaki dan u tjednu tako da vraćaju "ponedjeljak" za DayOfWeek.Monday, "utorak" za DayOfWeek.Tuesday itd.
 
+                // TODO:061 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
+                default:
+                    return "";
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static string RadniNeradni(DayOfWeek danUTjednu)
+        {
+            switch (danUTjednu)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    return "radni dan";
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    return "vikend";
+                // TODO:062 Napisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
                 // TODO:063 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
                 default:
                     return "";
