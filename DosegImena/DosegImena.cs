1c4c8a64a7347e600b3e6ecb742597080bdaad53
Julijan Sribar <julijan.sribar@gmail.com>
2023-11-09 19:57:13 +0100

    Resolve name scope

--- DosegImena/DosegImena.cs
+++ DosegImena/DosegImena.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Vsite.CSharp.KontrolaToka
 {
@@ -16,15 +17,16 @@ namespace Vsite.CSharp.KontrolaToka
         {
             string a = "Lokalna varijabla";
 
-            // TODO:000 Ispisati vrijednosti lokalne varijable a
-
-            // TODO:001 Ispisati vrijednost člana klase a
-
+            // 000 Ispisati vrijednosti lokalne varijable a
+            Console.WriteLine(a);
+            // 001 Ispisati vrijednost člana klase a
+            Console.WriteLine(this.a);
             {
-                // TODO:002 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
-
+                // :002 Definirati novu varijablu a unutar ovog bloka i ispisati njenu vrijednost
+                //int a = 5;
+                //for (int a = )
             }
-            // TODO:003 Pokrenuti testove i provjeriti je li prošao test u grupi TestDosegaImena
+            // :003 Pokrenuti testove i provjeriti je li prošao test u grupi TestDosegaImena
         }
 
         readonly string a = "Član klase";
