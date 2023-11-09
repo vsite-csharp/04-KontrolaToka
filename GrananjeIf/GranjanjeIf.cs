8c41c096a81c85e890ab55c0dfb7d467949e4cbf
Ivan Davidović <hedead421@gmail.com>
2023-11-09 20:07:22 +0100

    Resolve if branching

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -22,18 +22,34 @@
 
         public static void ProvjeriDjeljivost(int broj)
         {
-            // TODO:010 Napisati grananja if koja će za uneseni broj:
+            // :010 Napisati grananja if koja će za uneseni broj:
             // 1. provjeriti je li broj paran. 
             //    Ako je broj paran, treba ispisati poruku "broj je djeljiv s 2", a inače će
+            if (broj % 2 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv sa 2");
+            }
             // 2. provjeriti je li broj djeljiv s 3. 
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 3", a inače će
+            else if(broj % 3 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv sa 3");
+            }
             // 3. provjeriti je li broj djeljiv s 5. 
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 5", a inače
+            else if (broj % 5 == 0)
+            {
+                Console.WriteLine("Broj je dijeljiv sa 5");
+            }
             // 4. treba ispisati "broj nije djeljiv s 2, 3 niti 5"
+            else
+            {
+                Console.WriteLine("Broj nije dijeljiv sa 2, 3 niti 5");
+            }
 
-            // TODO:011 Pokrenuti program nekoliko puta i upisom različitih brojeva provjeriti ispravnost koda
+            // :011 Pokrenuti program nekoliko puta i upisom različitih brojeva provjeriti ispravnost koda
 
-            // TODO:012 Pokrenuti testove i provjeriti prolaze li 4 testa iz grupe TestGrananjaIf
+            // :012 Pokrenuti testove i provjeriti prolaze li 4 testa iz grupe TestGrananjaIf
         }
     }
 }
