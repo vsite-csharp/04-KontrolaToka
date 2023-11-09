ffb6da2ce3a07f4d2c7d70849e319e55403056a4
AnMal <junkie_@msn.com>
2023-11-09 20:05:02 +0100

    Resolved if branching

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -25,15 +25,30 @@
             // TODO:010 Napisati grananja if koja će za uneseni broj:
             // 1. provjeriti je li broj paran. 
             //    Ako je broj paran, treba ispisati poruku "broj je djeljiv s 2", a inače će
+            if (broj % 2 == 0)
+            {
+                Console.WriteLine("broj je djeljiv s 2");
+            }
             // 2. provjeriti je li broj djeljiv s 3. 
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 3", a inače će
+            else if (broj % 3 == 0)
+            {
+                Console.WriteLine("broj je djeljiv s 3");
+            }
             // 3. provjeriti je li broj djeljiv s 5. 
             //    Ako je broj djeljiv, treba ispisati poruku "broj je djeljiv s 5", a inače
+            else if (broj % 5 == 0)
+            {
+                Console.WriteLine("broj je djeljiv s 5");
+            }
             // 4. treba ispisati "broj nije djeljiv s 2, 3 niti 5"
+            else
+            {
+                Console.WriteLine("broj nije djeljiv s 2, 3 niti 5");
+            }
+            // :011 Pokrenuti program nekoliko puta i upisom različitih brojeva provjeriti ispravnost koda
 
-            // TODO:011 Pokrenuti program nekoliko puta i upisom različitih brojeva provjeriti ispravnost koda
-
-            // TODO:012 Pokrenuti testove i provjeriti prolaze li 4 testa iz grupe TestGrananjaIf
+            // :012 Pokrenuti testove i provjeriti prolaze li 4 testa iz grupe TestGrananjaIf
         }
     }
 }
