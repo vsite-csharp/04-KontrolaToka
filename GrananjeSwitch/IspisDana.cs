22c4a70554eea2e220a17aa7e690b1944f466ad4
Hrvoje Janđel <hrjandje@vsite.hr>
2023-11-09 21:06:09 +0100

    Resolve swich branching

--- GrananjeSwitch/IspisDana.cs
+++ GrananjeSwitch/IspisDana.cs
@@ -6,23 +6,40 @@
         {
             switch (danUTjednu)
             {
+                case DayOfWeek.Monday:
+                    return "ponedjeljak";
+
+                case DayOfWeek.Tuesday:
+                    return "utorak";
+                    
+            
                 // TODO:060 Napisati grane case za svaki dan u tjednu tako da vraćaju "ponedjeljak" za DayOfWeek.Monday, "utorak" za DayOfWeek.Tuesday itd.
 
                 // TODO:061 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
                 default:
-                    throw new NotImplementedException();
+                            return "";
+                //throw new ArgumentOutOfRangeException();
             }
         }
 
         public static string RadniNeradni(DayOfWeek danUTjednu)
         {
             switch (danUTjednu)
-            {
-                // TODO:062 Napisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
 
+            {
+                    case DayOfWeek.Monday:
+                    case DayOfWeek.Tuesday:
+                    case DayOfWeek.Wednesday:
+                    case  DayOfWeek.Thursday:
+                    case DayOfWeek.Friday:
+                        return "radni dan";
+                    // TODO:062 Napisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
+                    case DayOfWeek.Saturday:
+                    case DayOfWeek.Sunday:
+                    return "vikend";
                 // TODO:063 Za nepodržane vrijednosti treba baciti iznimku tipa ArgumentOutOfRangeException:
                 default:
-                    throw new NotImplementedException();
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
