2983b7cc5228120efc7f4fb66d68814075042dcd
Hrvoje Janđel <hrjandje@vsite.hr>
2023-11-09 21:21:11 +0100

    Resolve switch with paterns

--- SwitchSObrascima/GrananjeSwitchSObrascima.cs
+++ SwitchSObrascima/GrananjeSwitchSObrascima.cs
@@ -56,11 +56,20 @@ namespace Vsite.CSharp.KontrolaToka
         {
             switch (s.Godina)
             {
-                // TODO:070 Dodati case za studenta 1. godine koji će ispisati $"{s.Ime} je brucoš". Pokrenuti program i provjeriti ispis.
+                // :070 Dodati case za studenta 1. godine koji će ispisati $"{s.Ime} je brucoš". Pokrenuti program i provjeriti ispis.
+                case 1:
+                    Console.WriteLine($"{s.Ime} je brucoš");
+                    break;
 
                 // TODO:071 Dodati case za studente do 3. godine koji će ispisati $"{s.Ime} je student {s.Godina}. godine prediplomskog studija". Pokrenuti program i provjeriti ispis.
+                case  < 3:
+                    Console.WriteLine($"{s.Ime} je student {s.Godina}. godine prediplomskog studija");
+                    break;
 
                 // TODO:072 Dodati case za studente više od 3. godine koji će ispisati $"{s.Ime} je student {s.Godina - 3}. godine diplomskog studija". Pokrenuti program i provjeriti ispis.
+                case > 3:
+                    Console.WriteLine($"{s.Ime} je student {s.Godina - 3}. godine diplomskog studija");
+                    break;
             }
         }
 
@@ -68,11 +77,20 @@ namespace Vsite.CSharp.KontrolaToka
         {
             switch (o)
             {
+                case Student s when s.Godina>4:
+                    Console.WriteLine($"Student: {o.Ime}, {s.Godina}. godina");
+                    break;
+                case Student s:
+                    Console.WriteLine($"Student: {o.Ime}, {s.Godina}. godina");
+                    break;
                 // TODO:073 Dodati case za objekt tipa Osoba koji će ispisati $"Osoba: {o.Ime}". Pokrenuti program i provjeriti ispis.
-
+                case Osoba:
+                    Console.WriteLine($"Osoba: {o.Ime}");
+                    break;
                 // TODO:074 Dodati case za objekt tipa Student koji koji će ispisati $"Student: {o.Ime}, {s.Godina}. godina". Pokrenuti program i provjeriti ispis.
+               
 
-                // TODO:075 Dodati case za objekt tipa Student koji je na godini većoj od 4 koji će ispisati $"Student: {o.Ime} je diplomirao". Pokrenuti program i provjeriti ispis.
+                    // TODO:075 Dodati case za objekt tipa Student koji je na godini većoj od 4 koji će ispisati $"Student: {o.Ime} je diplomirao". Pokrenuti program i provjeriti ispis.
 
             }
         }
