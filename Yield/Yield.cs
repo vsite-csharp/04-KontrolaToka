e12a0bdb94110ccd4188f08a7b667828a53d202f
Josip Grabušić <jograbus@vsite.hr>
2023-11-16 20:47:10 +0100

    Resolve yield

--- Yield/Yield.cs
+++ Yield/Yield.cs
@@ -51,10 +51,17 @@
             {
                 // TODO:131 Promijenite petlju tako da se unutar petlje provjerava je li donja operacija bacila OverflowException i u tom slučaju prekida petlju
                 // U postavkama projekta uključiti opciju za provjeru numeričkog preljeva i pokrenuti program.
-                int zbroj = prethodni + trenutni;
-                prethodni = trenutni;
-                trenutni = zbroj;
-                rezultat.Add(trenutni);
+                try
+                {
+                    int zbroj = prethodni + trenutni;
+                    prethodni = trenutni;
+                    trenutni = zbroj;
+                    rezultat.Add(trenutni);
+                }
+                catch (OverflowException)
+                {
+                    break;
+                }
             }
             return rezultat;
         }
@@ -62,8 +69,32 @@
         // TODO:132 Implementirajte donju metodu korištenjem koda gornje metode FibonacciList
         public static IEnumerable<int> FibonacciIEnumerable()
         {
-            throw new NotImplementedException();
+            yield return 0;
+            yield return 1;
+
+          
+
+            int prethodni = 0;
+            int trenutni = 1;
+            while (prethodni < int.MaxValue)
+            {
+                
+                try
+                {
+                    int zbroj = prethodni + trenutni;
+                    prethodni = trenutni;
+                    trenutni = zbroj;
+                }
+                catch (OverflowException)
+                {
+                    yield break;
+                }
+            }
+            yield return trenutni;
         }
+
+            //throw new NotImplementedException();
+        
 
         // TODO:133 Pokrenuti program i provjeriti ispise
 
