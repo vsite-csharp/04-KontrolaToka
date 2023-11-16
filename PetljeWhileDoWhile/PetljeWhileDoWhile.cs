7e916659d87c857ce59dbb530618dfb0acee24c8
Hrvoje Janđel <hrjandje@vsite.hr>
2023-11-16 19:40:13 +0100

    Resolve do-while and while loops

--- PetljeWhileDoWhile/PetljeWhileDoWhile.cs
+++ PetljeWhileDoWhile/PetljeWhileDoWhile.cs
@@ -8,13 +8,13 @@
             int brojBacanja = 0;
             int bačeniBroj = 0;
 
-            // TODO:090 Napisati petlju koja ispisuje rezultat bacanja sve dok ne padne na 6
-
+            // :090 Napisati petlju koja ispisuje rezultat bacanja sve dok ne padne na 6
+            do
             {
                 ++brojBacanja;
                 bačeniBroj = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
                 Console.WriteLine($"{brojBacanja}. pokušaj: {bačeniBroj}");
-            }
+            } while (bačeniBroj!=6);
 
             return brojBacanja;
         }
@@ -23,10 +23,11 @@
         {
             Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
 
-            // TODO:091 Napisati petlju koja se ponavlja sve dok brojPređenihPolja ne postane jednak ili veći od 12
-
+            // :091 Napisati petlju koja se ponavlja sve dok brojPređenihPolja ne postane jednak ili veći od 12
+            while (brojPređenihPolja < 12)
             {
-                int bačeniBroj = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
+                
+                    int bačeniBroj = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
                 Console.WriteLine($"Bacanje: {bačeniBroj}");
                 brojPređenihPolja += bačeniBroj;
                 Console.WriteLine($"Ukupno: {brojPređenihPolja}");
@@ -34,11 +35,11 @@
             return brojPređenihPolja;
         }
 
-        // TODO:092 Pokrenuti program nekoliko puta i provjeriti ispise
+        // :092 Pokrenuti program nekoliko puta i provjeriti ispise
         static void Main()
         {
-
-            int brojPređenihPolja = BrojBacanjaDoBačeneŠestice();
+            
+                int brojPređenihPolja = BrojBacanjaDoBačeneŠestice();
 
             brojPređenihPolja = BacajDokNeProđe12Polja(brojPređenihPolja);
             Console.WriteLine($"Zbroj = {brojPređenihPolja}");
@@ -46,6 +47,6 @@
             Console.WriteLine("GOTOVO!!!");
         }
 
-        // TODO:093 Pokrenuti testove i provjeriti prolaze li testovi u grupi TestPetljeWhileDoWhile
+        // :093 Pokrenuti testove i provjeriti prolaze li testovi u grupi TestPetljeWhileDoWhile
     }
 }
