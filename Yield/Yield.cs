9ecaf70e79e48ab1e486661836b50759c39bb217
Iva Brletić <ivbrleti@vsite.hr>
2023-11-16 20:22:37 +0100

    Resolve for and while loop comperison

--- Yield/Yield.cs
+++ Yield/Yield.cs
@@ -47,14 +47,21 @@
 
             int prethodni = 0;
             int trenutni = 1;
-            while (prethodni < int.MaxValue / 2)
+            while (prethodni < int.MaxValue)
             {
                 // TODO:131 Promijenite petlju tako da se unutar petlje provjerava je li donja operacija bacila OverflowException i u tom slučaju prekida petlju
                 // U postavkama projekta uključiti opciju za provjeru numeričkog preljeva i pokrenuti program.
-                int zbroj = prethodni + trenutni;
-                prethodni = trenutni;
-                trenutni = zbroj;
-                rezultat.Add(trenutni);
+                try
+                {
+                    int zbroj = prethodni + trenutni;
+                    prethodni = trenutni;
+                    trenutni = zbroj;
+                    rezultat.Add(trenutni);
+                }
+                catch (OverflowException)
+                {
+                    break;
+                }
             }
             return rezultat;
         }
