473b1e6eb16fbf3ebf1f45872a62aafa3de5eb26
Mislav Ćavarušić <micavaru@vsite.hr>
2023-11-16 19:28:37 +0100

    Resolve for and while loop compare

--- ForVsWhile/ForVsWhile.cs
+++ ForVsWhile/ForVsWhile.cs
@@ -5,11 +5,21 @@
         public static void PetljaFor(int n)
         {
             // TODO:080 Napisati petlju for koja će ispisati brojeve od 0 do (uključivo) n
+            for (int i = 0; i<= n; ++i) 
+            {
+                Console.WriteLine(i);
+            }
         }
 
         public static void PetljaWhile(int n)
         {
             // TODO:081 Napisati petlju while koja će ispisati brojeve od 0 do (uključivo) n
+            int i = 0;
+            while (i <= n)
+            {
+                Console.WriteLine(i);
+                ++i;
+            }
         }
 
         static void Main(string[] args)
@@ -22,9 +32,9 @@
             Console.WriteLine("GOTOVO!!!");
         }
 
-        // TODO:082 Pokrenuti program te provjeriti ispise
+        // :082 Pokrenuti program te provjeriti ispise
 
-        // TODO:083 Programom ILDasm (Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N Tools\ildasm.exe) usporediti međukod metoda PetljaFor i PetljaWhile
+        // :083 Programom ILDasm (Program Files (x86)\Microsoft SDKs\Windows\vNN.N\bin\NETFX N.N Tools\ildasm.exe) usporediti međukod metoda PetljaFor i PetljaWhile
 
         // TODO:084 Pokrenuti testove i projeriti prolaze li oba testa u grupi TestForVsWhile
     }
