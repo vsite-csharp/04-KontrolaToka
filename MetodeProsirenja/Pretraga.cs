9a2d64b3f9fd60866e9ea79d2b07f1b28b182e19
DrHrast <petar.huljek@gmail.com>
2023-11-16 20:08:37 +0100

    Resolve IEnumerable expansion methods.

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -4,28 +4,43 @@
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:120 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            //foreach (var item in nizRiječi)
+            //{
+            //    if (item == tražena) { 
+            //        return true;
+            //    }
+            //}
+
+            //return false;
+
+            // DID_IT:120 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
+
+            return nizRiječi.Contains(tražena);
 
             throw new NotImplementedException();
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:121 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            // DID_IT:121 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+
+            return brojevi.Sum();
 
             throw new NotImplementedException();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:122 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            // DID_IT:122 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+
+            return nizRiječi.Where(clan => string.Compare(clan, graničnaRiječ, true) > 0);
 
             throw new NotImplementedException();
         }
 
-        // TODO:123 Pokrenuti program i provjeriti ispise
-        
-        // TODO:124 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        // DID_IT:123 Pokrenuti program i provjeriti ispise
+
+        // DID_IT:124 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
