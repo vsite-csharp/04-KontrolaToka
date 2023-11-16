a27814ec28abd1d3df39ef5c63d84e0ffa291676
Đuro Rukavina <djrukavina@vsite.hr>
2023-11-16 20:08:17 +0100

    Resolve IEnumerable extension methods

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -6,6 +6,12 @@
         {
             // TODO:120 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
+            //foreach(var rijec in nizRiječi)
+            //{
+            //    if (rijec == tražena) return true;
+            //}
+            //return false;
+            return nizRiječi.Contains(tražena);
 
             throw new NotImplementedException();
         }
@@ -13,6 +19,7 @@
         public static int Zbroj(IEnumerable<int> brojevi)
         {
             // TODO:121 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            return brojevi.Sum();
 
             throw new NotImplementedException();
         }
@@ -20,6 +27,7 @@
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
             // TODO:122 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            return nizRiječi.Where(clan => string.Compare(clan, graničnaRiječ, true) > 0);
 
             throw new NotImplementedException();
         }
