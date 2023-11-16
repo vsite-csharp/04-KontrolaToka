cbcfbe701e136a48b2f770f934b688b3af695c36
AnMal <junkie_@msn.com>
2023-11-16 20:08:10 +0100

    Resolve IEnumerable extension methods

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -4,28 +4,49 @@
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:120 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
+            // :120 Napisati kod koji će vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
             // (za jednostavnije rješenje, pogledati https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable)
 
-            throw new NotImplementedException();
+            //foreach (var rijec in nizRiječi)
+            //{
+            //    if (rijec == tražena)
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+
+            //return nizRiječi.Any(x => x == tražena);
+            return nizRiječi.Contains(tražena);
+
+            //throw new NotImplementedException();
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:121 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            // :121 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
+            
+            //var sum = 0;
+            //foreach (int i in brojevi)
+            //{
+            //    sum += i;
+            //}
+            //return sum;
 
-            throw new NotImplementedException();
+            return brojevi.Sum();
+            //throw new NotImplementedException();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:122 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
+            // :122 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
 
-            throw new NotImplementedException();
+            return nizRiječi.Where(x => string.Compare(x, graničnaRiječ, ignoreCase: true) > 0);
+            //throw new NotImplementedException();
         }
 
-        // TODO:123 Pokrenuti program i provjeriti ispise
+        // :123 Pokrenuti program i provjeriti ispise
         
-        // TODO:124 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
+        // :124 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestMetodaProširenja
     }
 }
