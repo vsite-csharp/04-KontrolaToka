7749ec2ba547a424d1c7e38380f1d5990f97c0d2
Ivan Fišić <ivfisic@vsite.hr>
2023-11-19 23:24:48 +0100

    Homework: Resolve break & continue

--- NaredbeBreakContinue/NaredbeBreakContinue.cs
+++ NaredbeBreakContinue/NaredbeBreakContinue.cs
@@ -7,8 +7,11 @@
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO:100 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
-
+                // 100 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                if (i % d != 0)
+                {
+                    continue;
+                }
                 zbroj += i;
             }
             return zbroj;
@@ -19,10 +22,17 @@
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO:101 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                // 101 Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+                if (i % d != 0)
+                {
+                    continue;
+                }
 
-
-                // TODO:102 Ako je i djeljiv sa d2, tada treba prekinuti petlju
+                // 102 Ako je i djeljiv sa d2, tada treba prekinuti petlju
+                if (i % d2 == 0)
+                {
+                    break;
+                }
 
 
                 zbroj += i;
@@ -30,7 +40,7 @@
             return zbroj;
         }
 
-        // TODO:103 Pokrenuti program i provjeriti ispise
+        // 103 Pokrenuti program i provjeriti ispise
 
         static void Main(string[] args)
         {
@@ -43,6 +53,6 @@
 
             Console.WriteLine("GOTOVO!!!");
         }
-        // TODO:104 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestNaredbeBreakContinue
+        // 104 Pokrenuti testove i provjeriti prolaze li svi testovi u grupi TestNaredbeBreakContinue
     }
 }
